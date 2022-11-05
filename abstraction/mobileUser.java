f7be71e4d68db373b469bf73db34ba808780ad98
rezwanrahimr <rezwanrahim69@gmail.com>
2022-11-05 22:47:51 +0600

    write non abstraction method in abstract class

--- abstraction/mobileUser.java
+++ abstraction/mobileUser.java
@@ -2,5 +2,9 @@ package abstraction;
 
 public abstract class mobileUser {
     
+    void call(){
+        System.out.println("This is call !");
+    }
+
    abstract void  sendMessage();
 }
--- abstraction/test.java
+++ abstraction/test.java
@@ -5,9 +5,11 @@ public class test {
     public static void main(String [] args){
         mobileUser us;
         us = new rahim();
+        us.call();
         us.sendMessage();
 
         us = new karim();
+        us.call();
         us.sendMessage();
     }
 }
