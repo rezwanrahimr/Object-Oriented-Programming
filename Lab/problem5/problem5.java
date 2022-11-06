aaca8cfc38c6552d35d3d9d775813c15640429e3
rezwanrahimr <rezwanrahim69@gmail.com>
2022-11-06 22:35:37 +0600

    determine the addition, subtraction, multiplication and division of two numbers using inheritance

---
+++ Lab/problem5/problem5.java
@@ -0,0 +1,28 @@
+package Lab.problem5;
+//  Write a java program to determine the addition, subtraction, multiplication and division of two numbers using inheritance.
+public class problem5 {
+    public void addition(int numberOne, int numberTwo){
+        int additionSum = numberOne + numberTwo;
+        System.out.println("Addition : " + additionSum);
+
+    }
+    
+    public void subtraction(int numberOne, int numberTwo){
+        int subtraction = numberOne - numberTwo;
+        System.out.println("subtraction : " + subtraction);
+    }
+
+    public void multiplication(int numberOne, int numberTwo){
+        int multiplication = numberOne * numberTwo;
+        System.out.println("multiplication : " + multiplication);
+    }
+
+    public void division(int numberOne, int numberTwo){
+        int division = numberOne / numberTwo;
+        System.out.println("division : " + division);
+    }
+
+
+
+
+}
---
+++ Lab/problem5/test.java
@@ -0,0 +1,12 @@
+package Lab.problem5;
+
+public class test extends problem5 {
+    
+    public static void main(String [] args){
+       test obj = new test();
+       obj.addition(10, 20);
+       obj.subtraction(30, 10);
+       obj.multiplication(20, 10);
+       obj.division(20, 10);
+    }
+}
