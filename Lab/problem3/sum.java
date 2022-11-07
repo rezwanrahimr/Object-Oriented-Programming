32a7de220c603f66d262df3ba876da173445dc18
rezwanrahimr <rezwanrahim69@gmail.com>
2022-11-07 22:31:43 +0600

    Write a java program to determine the area of a circle using class, object, and contractor

---
+++ Lab/problem3/sum.java
@@ -0,0 +1,23 @@
+package Lab.problem3;
+
+import javax.swing.JOptionPane;
+
+public class sum {
+     public static void main(String [] args){
+        int num,even = 0;
+        String number;
+        number = JOptionPane.showInputDialog("Enter Number !");
+        num = Integer.parseInt(number);
+        for(int i = 1; i <= num; i++){
+            if (i % 2 == 0) {
+                even = even + i;
+            }
+     
+          }
+
+          JOptionPane.showMessageDialog(null,"sum of all even numbers " + even);
+        
+          
+        }
+        
+}
---
+++ Lab/problem4/areaOfaCircle.java
@@ -0,0 +1,13 @@
+package Lab.problem4;
+
+public class areaOfaCircle {
+    areaOfaCircle(int radius){
+        double area = Math.PI*(radius*radius);
+        System.out.println("Area of Circle : " + area);
+    }
+
+    public static void main(String [] args){
+        areaOfaCircle obj = new areaOfaCircle(3);
+
+    }
+}
