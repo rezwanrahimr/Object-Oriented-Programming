f3951297b598b7750644674a125009ec8a1af015
rezwanrahimr <rezwanrahim69@gmail.com>
2022-11-07 18:18:19 +0600

    Write a java program to display Fibonacci using static method.

---
+++ Lab/Problem2/Fibonacci.java
@@ -0,0 +1,23 @@
+package Lab.Problem2;
+// Write a java program to display Fibonacci using static method.
+public class Fibonacci {
+    static void fibonaccii(int n){
+        int firstNumber = 0;
+        int secondNumber = 1;
+        int counter = 0;
+
+        while(counter < n){
+            System.out.println(firstNumber + " ");
+            int fibo = secondNumber + firstNumber;
+            firstNumber = secondNumber;
+            secondNumber = fibo;
+            counter++;
+           
+        }
+    }
+
+
+    public static void main(String [] args){
+        Fibonacci.fibonaccii(10);
+    }
+}
