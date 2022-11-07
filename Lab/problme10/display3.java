9d887ee6804f078621d8c1b98c06e0c8b6f36516
rezwanrahimr <rezwanrahim69@gmail.com>
2022-11-07 12:55:07 +0600

    Sequence of data type of arguments

---
+++ Lab/problme10/display3.java
@@ -0,0 +1,12 @@
+package Lab.problme10;
+//Sequence of data type of arguments.
+public class display3 {
+    public void dis(char a, int num){
+        System.out.println(("Char : " + a));
+        System.out.println(("Number : " + num));
+    }
+    public void dis(int num, char a){
+        System.out.println(("Char : " + a));
+        System.out.println(("Number : " + num));
+    }
+}
---
+++ Lab/problme10/testDisplay3.java
@@ -0,0 +1,9 @@
+package Lab.problme10;
+
+public class testDisplay3 {
+    public static void main(String [] args){
+        display3 obj = new display3();
+        obj.dis('A', 31);
+        obj.dis(31, 'A');
+    }
+}
