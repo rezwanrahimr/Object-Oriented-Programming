e29371d4628e99da57049cebacca7b526e3e3ce7
rezwanrahimr <rezwanrahim69@gmail.com>
2022-11-07 12:44:49 +0600

    Difference in data type of parameters.

---
+++ Lab/problme10/display2.java
@@ -0,0 +1,12 @@
+package Lab.problme10;
+//Difference in data type of parameters.
+public class display2 {
+    public void dis(int mobileNo, double time){
+        System.out.println("Mobile No : " + mobileNo);
+        System.out.println("Time : " + time);
+    }
+    public void dis(String address, String zipCode){
+        System.out.println("Address : " + address);
+        System.out.println("ZIP Code : " + zipCode);
+    }
+}
---
+++ Lab/problme10/testDisplay2.java
@@ -0,0 +1,9 @@
+package Lab.problme10;
+
+public class testDisplay2 {
+    public static void main(String [] args){
+        display2 obj = new display2();
+        obj.dis(0167333432, 12.41);
+        obj.dis("Dhaka,Bangladesh", "2343");
+    }
+}
